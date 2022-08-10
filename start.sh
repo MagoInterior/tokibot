5abaac6e2ce387647d7beb1f6a6000158c8a5cc8
SilasJr-dev <silasvljunior@gmail.com>
2022-08-10 13:02:18 -0300

    first commit

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 GREEN='\033[0;32m'
 while :
 do
-echo "${GREEN} Ashley - MD _ Auto reconexão para prevenção de quedas.."
+echo "${GREEN} Toki - MD _ Auto reconexão para prevenção de quedas.."
     node main.js
     sleep 1
 
